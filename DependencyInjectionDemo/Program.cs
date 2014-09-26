e5742da7af5a6b55e74444109b9e5cf7d9d63e56
ashafir <aviv@optimalfinance.co.il>
2014-09-26 14:51:55 +0300

    just more code and solution changes

---
+++ DependencyInjectionDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using NodaTime;
+
+namespace DependencyInjectionDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            IClock clock = SystemClock.Instance;
+
+            Licence licence = new Licence(new Instant(0), clock);
+
+            Diary diary = new Diary(clock, CalendarSystem.Iso, DateTimeZone.Utc);
+
+            DiaryPresenter presenter = new DiaryPresenter(diary, licence);
+
+            //these components are decoupled
+            //Its just a demonstration for how dependency injection
+            //can become complicated with many components involved...
+
+            //This is why we use a DI framework like Ninject...
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DependencyInjectionDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NodaTime;
+using NodaTime.Testing;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void FormatToday_Iso_Utc()
+        {
+            var clock = new FakeClock(new Instant(0));
+            Diary diary = new Diary(clock, CalendarSystem.Iso, DateTimeZone.Utc);
+
+            string today = diary.FormatToday();
+
+            Assert.AreEqual("1970-01-01", today);
+        }
+
+        [TestMethod]
+        public void FormatToday_Iso_NegativeOffset()
+        {
+            var zone = DateTimeZone.ForOffset(Offset.FromHours(-8));
+            var clock = new FakeClock(new Instant(0));
+            Diary diary = new Diary(clock, CalendarSystem.Iso, zone);
+
+            string today = diary.FormatToday();
+
+            Assert.AreEqual("1969-12-31", today);
+        }
+    }
+}
