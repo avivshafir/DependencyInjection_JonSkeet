be80a3ca13092b283e4192cbb848160903d30def
ashafir <aviv@optimalfinance.co.il>
2014-09-26 14:15:38 +0300

    init.

---
+++ DependencyInjection_JonSkeet/Diary.cs
@@ -0,0 +1,23 @@
+﻿using NodaTime;
+
+namespace DependencyInjection_JonSkeet
+{
+    public class Diary
+    {
+        private readonly IClock clock;
+        private readonly CalendarSystem calender;
+        private readonly DateTimeZone timeZone;
+
+        public Diary(IClock clock, CalendarSystem calender, DateTimeZone timeZone)
+        {
+            this.clock = clock;
+            this.calender = calender;
+            this.timeZone = timeZone;
+        }
+
+        public string DisplayToday()
+        {
+            
+        }
+    }
+}
